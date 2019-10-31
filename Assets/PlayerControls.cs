4e236f5ac7a384fedd5a12a45f95af42798552b1
majkii2115 <szymbaramichal@gmail.com>
2019-10-31 16:38:12 +0100

    31.10.2019

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,190 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PlayerControls : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""23de75a0-437f-4e67-96db-1c6d42c8f4f7"",
+            ""actions"": [
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""ed9f1e72-502e-4a4f-b561-2395cd68bfd8"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""9e8f312d-84bc-4e89-b095-1d30e2d75eb7"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotate"",
+                    ""type"": ""Button"",
+                    ""id"": ""024e0973-3c7f-41d2-b331-e93204872bd3"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""efb094f2-2678-4497-bdfa-35ef2ff99c64"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e8f18ee9-9e90-488b-abdb-60e06988b045"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e490d844-e611-459c-ae00-1bbdc190ccbc"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Attack = m_Gameplay.FindAction("Attack", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Rotate = m_Gameplay.FindAction("Rotate", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Attack;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Rotate;
+    public struct GameplayActions
+    {
+        private PlayerControls m_Wrapper;
+        public GameplayActions(PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Attack => m_Wrapper.m_Gameplay_Attack;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Rotate => m_Wrapper.m_Gameplay_Rotate;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                Attack.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                Attack.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                Attack.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnAttack;
+                Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                Rotate.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+                Rotate.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+                Rotate.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRotate;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Attack.started += instance.OnAttack;
+                Attack.performed += instance.OnAttack;
+                Attack.canceled += instance.OnAttack;
+                Move.started += instance.OnMove;
+                Move.performed += instance.OnMove;
+                Move.canceled += instance.OnMove;
+                Rotate.started += instance.OnRotate;
+                Rotate.performed += instance.OnRotate;
+                Rotate.canceled += instance.OnRotate;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnAttack(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+        void OnRotate(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using RPG.Combat;
+using UnityEngine.InputSystem;
 
 //HORIZONTAL - POZIOME
 //VERTICAL - PIONOWE
@@ -6,8 +8,16 @@ namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        public Joystick joystick;
+        PlayerControls controls;
+        // public Joystick joystick;
+        // joystick.Horizontal
+        // joystick.Vertical
         public float moveSpeed = 5f;
+        private void Awake() 
+        {
+            controls = new PlayerControls();
+            controls.Gameplay.Attack.performed += ctx => Attack();
+        }
         void Update()
         {
             float sum = VecSum();
@@ -15,14 +25,26 @@ namespace RPG.Control
             if(sum > 0.1f)
             {
                 gameObject.transform.eulerAngles = 
-                    new Vector3(0f, Vector3.SignedAngle(new Vector3(joystick.Horizontal ,0f, joystick.Vertical*(-1)), Vector3.forward, Vector3.up), 0f);
+                    new Vector3(0f, Vector3.SignedAngle(new Vector3(Input.GetAxis("Horizontal") ,0f, Input.GetAxis("Vertical")*(-1)), Vector3.forward, Vector3.up), 0f);
 
                 transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * sum);
             }
         }
+        void Attack()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
+        }
         public float VecSum()
         {
-            return Mathf.Sqrt(Mathf.Pow(joystick.Vertical, 2) + Mathf.Pow(joystick.Horizontal, 2));
+            return Mathf.Sqrt(Mathf.Pow(Input.GetAxis("Vertical"), 2) + Mathf.Pow(Input.GetAxis("Horizontal"), 2));
+        }
+        void OnEnable() 
+        {
+            controls.Gameplay.Enable();    
+        }
+        void OnDisable() 
+        {
+            controls.Gameplay.Disable();    
         }
     }
 }
